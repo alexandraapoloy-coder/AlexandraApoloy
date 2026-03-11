c688c32fdfb866aa3a7bad756f0fa53787422680
alexandraapoloy-coder <alexandraapoloy@gmail.com>
2026-03-11 23:58:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-return "Welcome to my Flask API!"
+    # The code inside the function MUST be indented
+    return "Welcome to my Flask API!"
 
 @app.route('/student')
 def get_student():
-return jsonify({
-"name": "Your Name",
-"grade": 10,
-"section": "Zechariah"
-})
+    return jsonify({
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
+    })
+
+if __name__ == "__main__":
+    app.run()
